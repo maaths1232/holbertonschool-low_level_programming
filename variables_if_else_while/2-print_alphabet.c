e116b9b6e5223f9f7148fd7a7eff5168f57172aa
root <feezmathys@gmail.com>
2025-06-19 12:11:37 +0000

    2-print_alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
  /**
   * main - point d'entrée du programme
-  * *
-  * * Description: Ce programme affiche toutes les lettres de l'alphabet
-  * * en minuscules, de 'a' à 'z', suivi d'un saut de ligne.
-  * * 
-  * * Contraintes:
-  * * - Utilisation uniquement de la fonction putchar pour l'affichage.
-  * * - Le code doit être contenu dans la fonction main.
-  * * - Putchar ne peut être utilisé que deux fois.
-  * *
-  * * Return: 0 pour indiquer que le programme s'est terminé avec succès.
-  * */
-int  main(void)
+  *
+  * Description: Ce programme affiche toutes les lettres de l'alphabet
+  * en minuscules, de 'a' à 'z', suivi d'un saut de ligne.
+  * 
+  * Contraintes:
+  * - Utilisation uniquement de la fonction putchar pour l'affichage.
+  * - Le code doit être contenu dans la fonction main.
+  * - Putchar ne peut être utilisé que deux fois.
+  *
+  * Return: 0 pour indiquer que le programme s'est terminé avec succès.
+  */
+int main(void)
 {
 	char c = 'a';
 	while (c<= 'z')
@@ -21,7 +21,7 @@ int  main(void)
 	putchar(c);
 	c++;
 	}
-	puthcar('\n');
+	putchar('\n');
 	
 	return 0;
 }
