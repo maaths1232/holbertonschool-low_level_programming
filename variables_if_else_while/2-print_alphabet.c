127cace54bc1842b1bed5adfaafd7847c164d51e
root <feezmathys@gmail.com>
2025-06-19 09:34:31 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+ /**
+  * main - point d'entrée du programme
+  * *
+  * * Description: Ce programme affiche toutes les lettres de l'alphabet
+  * * en minuscules, de 'a' à 'z', suivi d'un saut de ligne.
+  * * 
+  * * Contraintes:
+  * * - Utilisation uniquement de la fonction putchar pour l'affichage.
+  * * - Le code doit être contenu dans la fonction main.
+  * * - Putchar ne peut être utilisé que deux fois.
+  * *
+  * * Return: 0 pour indiquer que le programme s'est terminé avec succès.
+  * */
+main (void)
+{
+
