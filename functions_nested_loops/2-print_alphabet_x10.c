8edc25388caf91d6c62a0c5f2635d7925e78ca3b
root <feezmathys@gmail.com>
2025-06-20 16:46:40 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet in lowercase 10 times followed by a new line
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ * followed by a new line
  * You can only use _putchar twice in your code
  */
 void print_alphabet_x10(void)
 {
-    int i = 0;
-    char letter;
+	int i;
+	char letter;
 
-    while (i < 10)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-        i++;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
