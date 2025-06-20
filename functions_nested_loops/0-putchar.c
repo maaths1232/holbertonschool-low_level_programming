e995cf27e1a68db3c2fba17b33a888e83572fc57
root <feezmathys@gmail.com>
2025-06-20 13:35:16 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Point d'entrée du programme
+ * main - point d'entrée du programme
  *
  * Description: Ce programme affiche "_putchar" suivi d'un saut de ligne.
  *
- * Return: 0 pour indiquer une exécution réussie
+ * Return: 0 pour indiquer que le programme s'est terminé avec succès.
  */
 int main(void)
-	
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
