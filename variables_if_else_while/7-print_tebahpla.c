dd96785339a521ef5710893426fd1e22f7f4eac2
root <feezmathys@gmail.com>
2025-06-20 12:10:17 +0000

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche l'alphabet en minuscules à l'envers,
+ * suivi d'un saut de ligne.
+ * 
+ * Contraintes :
+ * - Utilisation uniquement de putchar (2 fois maximum).
+ * - Tout doit être dans la fonction main.
+ *
+ * Return: 0 si le programme s'exécute avec succès.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
