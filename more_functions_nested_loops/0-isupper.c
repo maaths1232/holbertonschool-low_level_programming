140c268037740ba16787990564338f74510001bd
root <feezmathys@gmail.com>
2025-06-24 09:23:05 +0000

    0-issuper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code.
+ * _isupper - Vérifie si un caractère est une majuscule (A-Z)
+ * @c: Le caractère à vérifier
  *
- * Return: Always 0.
+ * Return: 1 si majuscule, 0 sinon
  */
-int main(void)
-{
-        char c;
 
-        c = 'A'
-        printf ("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else 
+		return (0);
+
 }
--- more_functions_nested_loops/0-main.c
+++ more_functions_nested_loops/0-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-        char c;
+    char c;
 
-        c = 'A'
-        printf ("%c: %d\n", c, _isupper(c));
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
     c = 'a';
     printf("%c: %d\n", c, _isupper(c));
     return (0);
