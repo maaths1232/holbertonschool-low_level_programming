133791278541ea204ea3f0476cd5eb24b1201964
root <feezmathys@gmail.com>
2025-06-25 13:14:56 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - affiche les nombres de 1 à 100,
+ * remplace les multiples de 3 par "Fizz",
+ * les multiples de 5 par "Buzz",
+ * et les multiples de 3 et 5 par "FizzBuzz"
+ *
+ * Return: 0 (succès)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
